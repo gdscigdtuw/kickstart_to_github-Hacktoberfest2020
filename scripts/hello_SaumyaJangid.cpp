45e8a30072d549da3a07b8ed3b2fbdf778bc8a1c
Saumya Jangid <saumya10.jangid@gmail.com>
2020-10-23 20:55:15 +0530

    Create hello_SaumyaJangid.cpp

---
+++ scripts/hello_SaumyaJangid.cpp
@@ -0,0 +1,8 @@
+#include <iostream>                     //Header file for input output functions
+using namespace std;
+int main()                              //main function(where the execution of the file starts)
+{         
+    cout << "Hello world!" << endl;     //displays Hello world on the console
+    return 0;                           //return statement
+}
+                                        //program ends
