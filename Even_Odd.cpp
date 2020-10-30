8b12ab0eaa855069c133a927f84c0702b3ce08e3
surekhasangle <44473965+surekhasangle@users.noreply.github.com>
2020-10-30 18:31:01 +0530

    Create Even_Odd.cpp

---
+++ Even_Odd.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void printEvenNumbers(int N) 
+{ 
+
+	cout << "Even: "; 
+	for (int i = 1; i <= 2 * N; i++) { 
+
+		if (i % 2 == 0) 
+			cout << i << " "; 
+	} 
+} 
+
+void printOddNumbers(int N) 
+{ 
+
+	cout << "\nOdd: "; 
+	for (int i = 1; i <= 2 * N; i++) { 
+
+		// Numbers that are not divisible by 2 
+		if (i % 2 != 0) 
+			cout << i << " "; 
+	} 
+} 
+
+int main() 
+{ 
+
+	int N = 5; 
+
+	printEvenNumbers(N); 
+	printOddNumbers(N); 
+
+	return 0; 
+} 
